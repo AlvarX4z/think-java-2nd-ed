81852d291d9f2d42daaaaccbf9ea0cb2302241c1
Álvaro de Francisco <alfrasan@gmail.com>
2024-01-17 18:09:55 +0100

    Add Unit Test

--- src/test/java/org/alvarx4z/chapters/c02/TimeTest.java
+++ src/test/java/org/alvarx4z/chapters/c02/TimeTest.java
@@ -1,7 +1,11 @@
 package org.alvarx4z.chapters.c02;
 
+import org.alvarx4z.exceptions.InvalidHourNumberException;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -13,5 +17,15 @@ class TimeTest {
         final int response = Time.calculatePassedTimeSinceMidnight(10, 20, 30);
 
         assertThat(response).isBetween(Time.ZERO, Time.SECONDS_IN_A_DAY);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {-2, 25})
+    @DisplayName("Should throw InvalidHourNumberException if the given hour number is invalid")
+    void throwInvalidHourNumberException(int hour) {
+        Assertions.assertThrows(
+            InvalidHourNumberException.class,
+            () -> Time.calculatePassedTimeSinceMidnight(hour, 20, 30)
+        );
     }
 }
