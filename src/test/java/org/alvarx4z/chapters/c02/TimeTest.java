1240c858ec49e33384810eefb3254860035e827f
Álvaro de Francisco <alfrasan@gmail.com>
2024-01-17 18:19:22 +0100

    Small refactor

--- src/test/java/org/alvarx4z/chapters/c02/TimeTest.java
+++ src/test/java/org/alvarx4z/chapters/c02/TimeTest.java
@@ -3,13 +3,13 @@ package org.alvarx4z.chapters.c02;
 import org.alvarx4z.exceptions.InvalidHourNumberException;
 import org.alvarx4z.exceptions.InvalidMinuteNumberException;
 import org.alvarx4z.exceptions.InvalidSecondNumberException;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class TimeTest {
 
@@ -31,7 +31,7 @@ class TimeTest {
     @ValueSource(ints = {MINUS_TWO, 25})
     @DisplayName("Should throw InvalidHourNumberException if the given hour number is invalid")
     void throwInvalidHourNumberException(int hour) {
-        Assertions.assertThrows(
+        assertThrows(
             InvalidHourNumberException.class,
             () -> Time.calculatePassedTimeSinceMidnight(hour, TWENTY, THIRTY)
         );
@@ -41,7 +41,7 @@ class TimeTest {
     @ValueSource(ints = {MINUS_TWO, SIXTY_TWO})
     @DisplayName("Should throw InvalidMinuteNumberException if the given minute number is invalid")
     void throwInvalidMinuteNumberException(int minute) {
-        Assertions.assertThrows(
+        assertThrows(
             InvalidMinuteNumberException.class,
             () -> Time.calculatePassedTimeSinceMidnight(TEN, minute, THIRTY)
         );
@@ -51,7 +51,7 @@ class TimeTest {
     @ValueSource(ints = {MINUS_TWO, SIXTY_TWO})
     @DisplayName("Should throw InvalidSecondNumberException if the given second number is invalid")
     void throwInvalidSecondNumberException(int second) {
-        Assertions.assertThrows(
+        assertThrows(
             InvalidSecondNumberException.class,
             () -> Time.calculatePassedTimeSinceMidnight(TEN, TWENTY, second)
         );
