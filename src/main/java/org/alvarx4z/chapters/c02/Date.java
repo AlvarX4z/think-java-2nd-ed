ce441122f5ad33923539d50a17bee7ccb177a25c
Álvaro de Francisco <alfrasan@gmail.com>
2024-01-17 15:53:23 +0100

    Class Date

---
+++ src/main/java/org/alvarx4z/chapters/c02/Date.java
@@ -0,0 +1,14 @@
+package org.alvarx4z.chapters.c02;
+
+public class Date {
+
+    public static void exercise2() {
+        final String day = "Wednesday", month = "January";
+        final int date = 17, year = 2024;
+
+        System.out.println("| EXERCISE 02 |");
+        System.out.println("---------------");
+        System.out.printf("American format: %s, %s %s, %s\n", day, month, date, year);
+        System.out.printf("European format: %s %s %s %s\n", day, date, month, year);
+    }
+}
