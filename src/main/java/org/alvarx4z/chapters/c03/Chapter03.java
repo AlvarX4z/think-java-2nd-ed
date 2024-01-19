32f063cd34491627d9a3d2c87aa98309c7c24ffc
Álvaro de Francisco <alfrasan@gmail.com>
2024-01-19 14:28:10 +0100

    Chapter03 Class

---
+++ src/main/java/org/alvarx4z/chapters/c03/Chapter03.java
@@ -0,0 +1,15 @@
+package org.alvarx4z.chapters.c03;
+
+public final class Chapter03 {
+
+    public static void execute() {
+        presentation();
+        CelsiusToFahrenheitCalculator.exercise2();
+    }
+
+    private static void presentation() {
+        System.out.println("\n==============");
+        System.out.println("| CHAPTER 03 |");
+        System.out.println("==============\n");
+    }
+}
