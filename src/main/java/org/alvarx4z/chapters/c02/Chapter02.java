1526115ccf2661b77e421446d1cf81ff5a222cc9
Álvaro de Francisco <alfrasan@gmail.com>
2024-01-19 11:30:18 +0100

    Chapter02

---
+++ src/main/java/org/alvarx4z/chapters/c02/Chapter02.java
@@ -0,0 +1,16 @@
+package org.alvarx4z.chapters.c02;
+
+public final class Chapter02 {
+
+    public static void execute() {
+        presentation();
+        Date.exercise2();
+        Time.exercise3();
+    }
+
+    private static void presentation() {
+        System.out.println("\n==============");
+        System.out.println("| CHAPTER 02 |");
+        System.out.println("==============\n");
+    }
+}
