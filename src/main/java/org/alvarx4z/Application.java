749512d89504c0753e95d396a01be0678d797256
Álvaro de Francisco <alfrasan@gmail.com>
2024-01-19 12:32:11 +0100

    Refactor

--- src/main/java/org/alvarx4z/Application.java
+++ src/main/java/org/alvarx4z/Application.java
@@ -1,9 +1,12 @@
 package org.alvarx4z;
 
 import org.alvarx4z.chapters.c01.Chapter01;
+import org.alvarx4z.chapters.c02.Chapter02;
 
-public class Application {
+public final class Application {
+
     public static void main(String[] args) {
-        Chapter01.exercise2();
+        Chapter01.execute();
+        Chapter02.execute();
     }
 }
--- src/main/java/org/alvarx4z/chapters/c01/Chapter01.java
+++ src/main/java/org/alvarx4z/chapters/c01/Chapter01.java
@@ -1,10 +1,21 @@
 package org.alvarx4z.chapters.c01;
 
-public class Chapter01 {
-    public static void exercise2() {
+public final class Chapter01 {
+
+    public static void execute() {
+        presentation();
+        exercise2();
+    }
+
+    private static void presentation() {
         System.out.println("\n==============");
         System.out.println("| CHAPTER 01 |");
-        System.out.println("==============");
+        System.out.println("==============\n");
+    }
+
+    private static void exercise2() {
+        System.out.println("| EXERCISE 02 |");
+        System.out.println("---------------");
         System.out.println("Hello, World!");
         System.out.println("How are you?\n");
     }
