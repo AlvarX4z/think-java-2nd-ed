cc545597e706f66843646735e345b24798c5b118
Álvaro de Francisco <alfrasan@gmail.com>
2024-01-19 14:28:40 +0100

    Call Chapter03

--- src/main/java/org/alvarx4z/Application.java
+++ src/main/java/org/alvarx4z/Application.java
@@ -2,11 +2,13 @@ package org.alvarx4z;
 
 import org.alvarx4z.chapters.c01.Chapter01;
 import org.alvarx4z.chapters.c02.Chapter02;
+import org.alvarx4z.chapters.c03.Chapter03;
 
 public final class Application {
 
     public static void main(String[] args) {
         Chapter01.execute();
         Chapter02.execute();
+        Chapter03.execute();
     }
 }
